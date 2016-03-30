fe6f53d6f2b8e002754f2f29e46fa56116525374
williamhickok11 <williamhickok11@gmail.com>
2016-03-30 14:50:50 -0500

    working on adding j k l images

--- app/js/main.js
+++ app/js/main.js
@@ -37,11 +37,15 @@ function createGame() {
 
 // Preload assets
 function preload() {
+	// Load background image
+	game.load.image('background', './app/img/assets/Background.png');
 	// Load text image
 	game.load.bitmapFont('desyrel', './app/img/assets/desyrel.png', './app/img/assets/desyrel.xml');
 	// Load note image
 	var dir = 'app/img/assets/';
-	game.load.image('laser', dir + 'laserBlue02.png');
+	game.load.image('laser1', dir + 'J_image.png');
+	game.load.image('laser2', dir + 'K_image.png');
+	game.load.image('laser3', dir + 'L_image.png');
 	game.load.image('button', './app/img/assets/start.png');
 }
 var scoreText;
@@ -58,6 +62,8 @@ function init() {
 
 // Assets are available in create
 function create() {
+
+	game.add.image(game.world.centerX, game.world.centerY, 'background').anchor.set(0.5);
 	// Create the game score and multiplier
 	scoreText = game.add.bitmapText(120, 60, 'desyrel','Phaser & Pixi \nrocking!', 44);
 	multiplierText = game.add.bitmapText(120, 110, 'desyrel','Phaser & Pixi \nrocking!', multiplierFontSize);
@@ -80,9 +86,9 @@ function create() {
 		This way we save on precious resources by not constantly adding & removing new sprites to the stage
 	*/
 
-	lasersBlue.createMultiple(20, 'laser');
-	lasersRed.createMultiple(20, 'laser');
-	lasersGreen.createMultiple(20, 'laser');
+	lasersBlue.createMultiple(90, 'laser1');
+	lasersRed.createMultiple(90, 'laser2');
+	lasersGreen.createMultiple(90, 'laser3');
 	lasersBlue.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetLaser);
 	lasersRed.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetLaser);
 	lasersGreen.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetLaser);
@@ -100,14 +106,28 @@ function create() {
 
 // Start Game
 function startGame () {
-	fireBlueLaser();
-	// Load audio
+				// 	// TEST LASERS
+				// fireBlueLaser();
+				// setTimeout(function() { fireBlueLaser(); }, 1000);
+				// setTimeout(function() { fireBlueLaser(); }, 2000);
+				// setTimeout(function() { fireBlueLaser(); }, 3000);
+				// setTimeout(function() { fireBlueLaser(); }, 4000);
+				// setTimeout(function() { fireBlueLaser(); }, 5000);
 	button.kill();
+	// Load audio
 	audio_Bg = new Audio("./app/audio/Zionexx_Guitar_Hero.wav");
 	audio_Melody = new Audio("./app/audio/Zionexx_Guitar_Hero_Melody.wav");
-	audio_applause = new Audio("./app/audio/applause.wav");
-	audio_applause.volume = 0.6;
+	audio_Wrong_Note1 = new Audio("./app/audio/missedNotes/Missed_Note1.wav");
+	audio_Wrong_Note2 = new Audio("./app/audio/missedNotes/Missed_Note2.wav");
+	audio_Wrong_Note3 = new Audio("./app/audio/missedNotes/Missed_Note3.wav");
+	audio_Wrong_Note4 = new Audio("./app/audio/missedNotes/Missed_Note4.wav");
 	audio_boo = new Audio("./app/audio/boo.wav");
+	audio_applause = new Audio("./app/audio/applause.wav");
+	audio_Wrong_Note1.volume = 0.25;
+	audio_Wrong_Note2.volume = 0.25;
+	audio_Wrong_Note3.volume = 0.25;
+	audio_Wrong_Note4.volume = 0.25;
+	audio_applause.volume = 0.6;
 	// Play audio
 	audio_Melody.oncanplaythrough = function(){
 		setTimeout(function() {
@@ -134,12 +154,15 @@ function update() {
 			// Calculate score with the current note
 			if (key.keyCode === 74) {
 				calScore(lasersBlue);
+				// checkWrongNote(lasersBlue);
 			};
 			if (key.keyCode === 75) {
 				calScore(lasersRed);
+				// checkWrongNote(lasersRed);
 			};
 			if (key.keyCode === 76) {
 				calScore(lasersGreen);
+				// checkWrongNote(lasersGreen);
 			};
 		}
 	}
@@ -176,7 +199,7 @@ function audioFadeOut(currSong) {
 }
 
 // Reset laser when the laser goes out of bounds (missed a note)
-function resetLaser(laser) {
+function resetLaser(laser, wrongNoteCheck) {
 	// console.log("laser", laser);
 	multiplier = 1;
 	// check for the number of missed notes and fire boo audio
@@ -190,7 +213,6 @@ function resetLaser(laser) {
 	if (!audio_applause.paused) {
 		audioFadeOut(audio_applause);
 	};
-	mute_song();
 	// Save the highest streak for the current game
 	if (currentStreak > currentHighStreak) {
 		currentHighStreak = currentStreak;
@@ -198,10 +220,14 @@ function resetLaser(laser) {
 	};
 	// Reset currentStreak
 	currentStreak = 0;
-	// remove the note from the page
-	laser.kill();
-}
 
+	if (wrongNoteCheck !== false) {
+		// Mute song as if you weren't playing
+		mute_song();
+		// remove the note from the page
+		laser.kill();
+	};
+}
 
 function dealWithCorrectNotes(thisNote) {
 	booCounter = 0;
@@ -211,7 +237,7 @@ function dealWithCorrectNotes(thisNote) {
 	// Enable music for correct notes
 	audio_Melody.muted = false;
 	// Kill the note so it doesn't go out of bounds
-	thisNote.kill();
+	thisNote.destroy();
 	// remove missed not visual
 	$("#game_container").removeClass("missedNoteBoarder");
 }
@@ -250,20 +276,42 @@ function calmultiplyer() {
 
 // Calculate Score
 function calScore(curNote) {
+	var didHit = false;
 	// console.log("curNote", curNote);
 	for (var i = 0; i < curNote.children.length; i++) {
 		// Check the position of the note
 		if (curNote.children[i].y > GAME_HEIGHT - 75 && curNote.children[i].y < GAME_HEIGHT - 25) {
 			console.log("curNote.children[i].y",curNote.children[i].y);
 			playerScore += (50 * multiplier);
+			didHit = true;
 			dealWithCorrectNotes(curNote.children[i]);
-			console.log("what is happening");
 		} else if (curNote.children[i].y > GAME_HEIGHT - 100 && curNote.children[i].y < GAME_HEIGHT) {
 			console.log("curNote.children[i].y",curNote.children[i].y);
 			playerScore += (25 * multiplier);
+			didHit = true;
 			dealWithCorrectNotes(curNote.children[i]);
-			console.log("what is happening");
-		}
+		};
+	};
+	// Deal with playing the wrong note
+	if (didHit === false) {
+		var random = Math.random();
+		// Play wrong note sound
+		switch (true) {
+			case (random < 0.25):
+				audio_Wrong_Note1.play();
+				break;
+			case (random >= 0.25 && random < 0.5):
+				audio_Wrong_Note2.play();
+				break;
+			case (random >= 0.5 && random < 0.75):
+				audio_Wrong_Note3.play();
+				break;
+			case (random >= 0.75):
+				audio_Wrong_Note4.play();
+				break;
+		};
+		resetLaser(curNote, false);
+		console.log("you played the wrong note");
 	};
 }
 
@@ -272,23 +320,25 @@ function fireBlueLaser() {
 	var laser = lasersRed.getFirstExists(false);
 	if (laser) {
 		// If we have a laser, set it to the starting position
-		laser.reset(200, 0);
+		laser.reset(225, 220);
 		// Give it a velocity of 500 so it starts moving
-		laser.body.velocity.y = 500;
+		laser.body.velocity.y = 280;
 	}
 }
 function fireGreenLaser() {
 	var laser = lasersBlue.getFirstExists(false);
 	if (laser) {
-		laser.reset(50, 0);
-		laser.body.velocity.y = 500;
+		laser.reset(205, 220);
+		laser.body.velocity.y = 280;
+		laser.body.velocity.x = -85;
 	}
 }
 function fireRedLaser() {
 	var laser = lasersGreen.getFirstExists(false);
 	if (laser) {
-		laser.reset(400, 0);
-		laser.body.velocity.y = 500;
+		laser.reset(250, 220);
+		laser.body.velocity.y = 280;
+		laser.body.velocity.x = 85;
 	}
 }
 
--- app/js/song.js
+++ app/js/song.js
@@ -1,209 +1,209 @@
-// // setTimeout(function() { fireRedLaser(); }, 2500);
-// // setTimeout(function() { fireBlueLaser(); }, 7000);
-// // setTimeout(function() { fireGreenLaser(); }, 3000);
+// setTimeout(function() { fireRedLaser(); }, 2500);
+// setTimeout(function() { fireBlueLaser(); }, 7000);
+// setTimeout(function() { fireGreenLaser(); }, 3000);
 
-// // INTRO //
-// // A.a //
+// INTRO //
+// A.a //
 
-// var fireSong = function() {
-//   var sectionA = 0;
-//   var introAa = function () {
-//     setTimeout(function() { fireRedLaser(); }, 000);
-//     setTimeout(function() { fireBlueLaser(); }, 250);
-//     setTimeout(function() { fireGreenLaser(); }, 500);
+var fireSong = function() {
+  var sectionA = 0;
+  var introAa = function () {
+    setTimeout(function() { fireRedLaser(); }, 000);
+    setTimeout(function() { fireBlueLaser(); }, 250);
+    setTimeout(function() { fireGreenLaser(); }, 500);
 
-//     setTimeout(function() { fireRedLaser(); }, 1500);
-//     setTimeout(function() { fireGreenLaser(); }, 1750);
-//     setTimeout(function() { fireBlueLaser(); }, 2000);
+    setTimeout(function() { fireRedLaser(); }, 1500);
+    setTimeout(function() { fireGreenLaser(); }, 1750);
+    setTimeout(function() { fireBlueLaser(); }, 2000);
 
-//     setTimeout(function() { fireBlueLaser(); }, 3500);
-//     setTimeout(function() { fireGreenLaser(); }, 3750);
-//     setTimeout(function() { fireRedLaser(); }, 4000);
-//     setTimeout(function() { fireBlueLaser(); }, 4250);
-//     setTimeout(function() { fireGreenLaser(); }, 4500);
+    setTimeout(function() { fireBlueLaser(); }, 3500);
+    setTimeout(function() { fireGreenLaser(); }, 3750);
+    setTimeout(function() { fireRedLaser(); }, 4000);
+    setTimeout(function() { fireBlueLaser(); }, 4250);
+    setTimeout(function() { fireGreenLaser(); }, 4500);
 
-//     setTimeout(function() { fireBlueLaser(); }, 5500);
-//     setTimeout(function() { fireGreenLaser(); }, 5750);
-//     setTimeout(function() { fireRedLaser(); }, 6000);
-//     setTimeout(function() { fireBlueLaser(); }, 6250);
-//     setTimeout(function() { fireGreenLaser(); }, 6500);
+    setTimeout(function() { fireBlueLaser(); }, 5500);
+    setTimeout(function() { fireGreenLaser(); }, 5750);
+    setTimeout(function() { fireRedLaser(); }, 6000);
+    setTimeout(function() { fireBlueLaser(); }, 6250);
+    setTimeout(function() { fireGreenLaser(); }, 6500);
 
-//     if (sectionA === 1) {
-//       return;
-//     };
+    if (sectionA === 1) {
+      return;
+    };
 
-//     setTimeout(function() { fireBlueLaser(); }, 6750);
-//     setTimeout(function() { fireGreenLaser(); }, 7000);
-//     setTimeout(function() { fireBlueLaser(); }, 7250);
-//     setTimeout(function() { fireRedLaser(); }, 7500);
-//     setTimeout(function() { fireBlueLaser(); }, 7750);
-//     sectionA++;
-//   };
+    setTimeout(function() { fireBlueLaser(); }, 6750);
+    setTimeout(function() { fireGreenLaser(); }, 7000);
+    setTimeout(function() { fireBlueLaser(); }, 7250);
+    setTimeout(function() { fireRedLaser(); }, 7500);
+    setTimeout(function() { fireBlueLaser(); }, 7750);
+    sectionA++;
+  };
 
-//   // FIRE Section A.a //
-//   setTimeout(function() { introAa(); }, 7000);
-//   setTimeout(function() { introAa(); }, 15000);
+  // FIRE Section A.a //
+  setTimeout(function() { introAa(); }, 7000);
+  setTimeout(function() { introAa(); }, 15000);
 
-//   // A.b
-//   var introAb = function () {
-//     setTimeout(function() { fireBlueLaser(); }, 500);
-//     setTimeout(function() { fireGreenLaser(); }, 750);
-//     setTimeout(function() { fireRedLaser(); }, 1000);
-//     setTimeout(function() { fireBlueLaser(); }, 1250);
-//     setTimeout(function() { fireGreenLaser(); }, 1500);
-//     setTimeout(function() { fireRedLaser(); }, 1750);
-//     setTimeout(function() { fireBlueLaser(); }, 2000);
-//     setTimeout(function() { fireGreenLaser(); }, 2250);
-//   }
-//   // FIRE Section A.b //
-//   setTimeout(function() { introAb(); }, 22000);
-//   setTimeout(function() { introAb(); }, 24000);
-//   setTimeout(function() { introAb(); }, 26000);
-//   setTimeout(function() { introAb(); }, 28000);
-
-
-//   // Drop
-//   var drop = function () {
-//     var sectionB = 0;
-//     // Drop.a
-//     function drop_a_a() {
-//       setTimeout(function() { fireRedLaser(); }, 00);
-//       setTimeout(function() { fireGreenLaser(); }, 00);
-
-//       setTimeout(function() { fireRedLaser(); }, 500);
-//       setTimeout(function() { fireBlueLaser(); }, 625);
-//       setTimeout(function() { fireGreenLaser(); }, 750);
-//       setTimeout(function() { fireBlueLaser(); }, 875);
-//       setTimeout(function() { fireRedLaser(); }, 1000);
-//       setTimeout(function() { fireBlueLaser(); }, 1625);
-//       setTimeout(function() { fireGreenLaser(); }, 1625);
-//     }
-//     drop_a_a();
-
-//     function drop_a_b() {
-//       setTimeout(function() { fireRedLaser(); }, 2000);
-//       setTimeout(function() { fireBlueLaser(); }, 2125);
-//       setTimeout(function() { fireGreenLaser(); }, 2250);
-//       setTimeout(function() { fireBlueLaser(); }, 2375);
-//       setTimeout(function() { fireGreenLaser(); }, 2500);
-//       setTimeout(function() { fireGreenLaser(); }, 2750);
-
-//       if (sectionB === 1) {
-//         return;
-//       };
-//       setTimeout(function() { fireGreenLaser(); }, 3000);
-//       setTimeout(function() { fireRedLaser(); }, 3000);
-
-//       setTimeout(function() { fireGreenLaser(); }, 3250);
-//       setTimeout(function() { fireBlueLaser(); }, 3250);
-
-//       setTimeout(function() { fireGreenLaser(); }, 3500);
-//       setTimeout(function() { fireRedLaser(); }, 3500);
-
-//       setTimeout(function() { fireGreenLaser(); }, 3750);
-//       setTimeout(function() { fireBlueLaser(); }, 3750);
-
-//       // Drop.b
-//       setTimeout(function() { drop_a_a(); }, 4000);
-//       // Continue Drop.b.b
-//       setTimeout(function() { fireRedLaser(); }, 6000);
-//       setTimeout(function() { fireGreenLaser(); }, 6500);
-//       setTimeout(function() { fireGreenLaser(); }, 6750);
-//       setTimeout(function() { fireBlueLaser(); }, 7000);
-//       setTimeout(function() { fireBlueLaser(); }, 7167);
-//       setTimeout(function() { fireBlueLaser(); }, 7333);
-//       setTimeout(function() { fireGreenLaser(); }, 7500);
-//       sectionB++;
-//     }
-//     drop_a_b();
+  // A.b
+  var introAb = function () {
+    setTimeout(function() { fireBlueLaser(); }, 500);
+    setTimeout(function() { fireGreenLaser(); }, 750);
+    setTimeout(function() { fireRedLaser(); }, 1000);
+    setTimeout(function() { fireBlueLaser(); }, 1250);
+    setTimeout(function() { fireGreenLaser(); }, 1500);
+    setTimeout(function() { fireRedLaser(); }, 1750);
+    setTimeout(function() { fireBlueLaser(); }, 2000);
+    setTimeout(function() { fireGreenLaser(); }, 2250);
+  }
+  // FIRE Section A.b //
+  setTimeout(function() { introAb(); }, 22000);
+  setTimeout(function() { introAb(); }, 24000);
+  setTimeout(function() { introAb(); }, 26000);
+  setTimeout(function() { introAb(); }, 28000);
 
 
-// ////////////////////////////////////////////////////////////////////////
-//     setTimeout(function() { fireGreenLaser(); }, 8000);
-//     setTimeout(function() { fireGreenLaser(); }, 8500);
-//     setTimeout(function() { fireGreenLaser(); }, 9000);
-//     setTimeout(function() { fireGreenLaser(); }, 9500);
-// ////////////////////////////////////////////////////////////////////////
+  // Drop
+  var drop = function () {
+    var sectionB = 0;
+    // Drop.a
+    function drop_a_a() {
+      setTimeout(function() { fireRedLaser(); }, 00);
+      setTimeout(function() { fireGreenLaser(); }, 00);
 
-//     setTimeout(function() { fireGreenLaser(); }, 10000);
-//     setTimeout(function() { fireBlueLaser(); }, 10000);
+      setTimeout(function() { fireRedLaser(); }, 500);
+      setTimeout(function() { fireBlueLaser(); }, 625);
+      setTimeout(function() { fireGreenLaser(); }, 750);
+      setTimeout(function() { fireBlueLaser(); }, 875);
+      setTimeout(function() { fireRedLaser(); }, 1000);
+      setTimeout(function() { fireBlueLaser(); }, 1625);
+      setTimeout(function() { fireGreenLaser(); }, 1625);
+    }
+    drop_a_a();
 
-//     setTimeout(function() { fireRedLaser(); }, 10500);
-//     setTimeout(function() { fireBlueLaser(); }, 10625);
-//     setTimeout(function() { fireGreenLaser(); }, 10750);
-//     setTimeout(function() { fireBlueLaser(); }, 10875);
-//     setTimeout(function() { fireRedLaser(); }, 11000);
-//     setTimeout(function() { fireBlueLaser(); }, 11167);
-//     setTimeout(function() { fireBlueLaser(); }, 11333);
-//     setTimeout(function() { fireGreenLaser(); }, 11500);
+    function drop_a_b() {
+      setTimeout(function() { fireRedLaser(); }, 2000);
+      setTimeout(function() { fireBlueLaser(); }, 2125);
+      setTimeout(function() { fireGreenLaser(); }, 2250);
+      setTimeout(function() { fireBlueLaser(); }, 2375);
+      setTimeout(function() { fireGreenLaser(); }, 2500);
+      setTimeout(function() { fireGreenLaser(); }, 2750);
+
+      if (sectionB === 1) {
+        return;
+      };
+      setTimeout(function() { fireGreenLaser(); }, 3000);
+      setTimeout(function() { fireRedLaser(); }, 3000);
+
+      setTimeout(function() { fireGreenLaser(); }, 3250);
+      setTimeout(function() { fireBlueLaser(); }, 3250);
+
+      setTimeout(function() { fireGreenLaser(); }, 3500);
+      setTimeout(function() { fireRedLaser(); }, 3500);
+
+      setTimeout(function() { fireGreenLaser(); }, 3750);
+      setTimeout(function() { fireBlueLaser(); }, 3750);
+
+      // Drop.b
+      setTimeout(function() { drop_a_a(); }, 4000);
+      // Continue Drop.b.b
+      setTimeout(function() { fireRedLaser(); }, 6000);
+      setTimeout(function() { fireGreenLaser(); }, 6500);
+      setTimeout(function() { fireGreenLaser(); }, 6750);
+      setTimeout(function() { fireBlueLaser(); }, 7000);
+      setTimeout(function() { fireBlueLaser(); }, 7167);
+      setTimeout(function() { fireBlueLaser(); }, 7333);
+      setTimeout(function() { fireGreenLaser(); }, 7500);
+      sectionB++;
+    }
+    drop_a_b();
 
 
-//     // Drop.c
+////////////////////////////////////////////////////////////////////////
+    setTimeout(function() { fireGreenLaser(); }, 8000);
+    setTimeout(function() { fireGreenLaser(); }, 8500);
+    setTimeout(function() { fireGreenLaser(); }, 9000);
+    setTimeout(function() { fireGreenLaser(); }, 9500);
+////////////////////////////////////////////////////////////////////////
 
-//   }
+    setTimeout(function() { fireGreenLaser(); }, 10000);
+    setTimeout(function() { fireBlueLaser(); }, 10000);
 
-//   setTimeout(function() { drop(); }, 31000);
-//   setTimeout(function() {  drop(); }, 47000);
+    setTimeout(function() { fireRedLaser(); }, 10500);
+    setTimeout(function() { fireBlueLaser(); }, 10625);
+    setTimeout(function() { fireGreenLaser(); }, 10750);
+    setTimeout(function() { fireBlueLaser(); }, 10875);
+    setTimeout(function() { fireRedLaser(); }, 11000);
+    setTimeout(function() { fireBlueLaser(); }, 11167);
+    setTimeout(function() { fireBlueLaser(); }, 11333);
+    setTimeout(function() { fireGreenLaser(); }, 11500);
 
 
-//   // OUTRO
-//   function outro() {
-//     var lastPhrase;
-//     function dblbeat_1() {
-//       setTimeout(function() { fireGreenLaser(); }, 00);
-//       setTimeout(function() { fireBlueLaser(); }, 125);
-//     }
+    // Drop.c
 
-//     function dblbeat_2() {
-//       setTimeout(function() { fireBlueLaser(); }, 00);
-//       setTimeout(function() { fireRedLaser(); }, 125);
-//     }
+  }
 
-//     function dblbeat_3() {
-//       setTimeout(function() { fireRedLaser(); }, 00);
-//       setTimeout(function() { fireGreenLaser(); }, 125);
-//     }
+  setTimeout(function() { drop(); }, 31000);
+  setTimeout(function() {  drop(); }, 47000);
 
-//     // Reuse this code to keep it DRY
-//     function fire_dblbeats(currDblBeat, num) {
-//       setTimeout(function() { fireBlueLaser(); }, 250);
 
-//       setTimeout(function() { currDblBeat(); }, 500);
-//       if (num === 0) {
-//         setTimeout(function() { currDblBeat(); }, 875);
-//       };
-//       setTimeout(function() { currDblBeat(); }, 1250);
-//       setTimeout(function() { currDblBeat(); }, 1625);
-//     }
+  // OUTRO
+  function outro() {
+    var lastPhrase;
+    function dblbeat_1() {
+      setTimeout(function() { fireGreenLaser(); }, 00);
+      setTimeout(function() { fireBlueLaser(); }, 125);
+    }
 
-//     setTimeout(function() { fire_dblbeats(dblbeat_1, 0); }, 0);
-//     setTimeout(function() { fire_dblbeats(dblbeat_2, 0); }, 2000);
-//     setTimeout(function() { fire_dblbeats(dblbeat_3, 1); }, 4000);
+    function dblbeat_2() {
+      setTimeout(function() { fireBlueLaser(); }, 00);
+      setTimeout(function() { fireRedLaser(); }, 125);
+    }
 
-//     // Continue to the second phrase
-//     setTimeout(function() { fireBlueLaser(); }, 5000);
-//     setTimeout(function() { fireGreenLaser(); }, 5250);
-//     setTimeout(function() { fireRedLaser(); }, 5500);
-//     setTimeout(function() { fireBlueLaser(); }, 5750);
-//     setTimeout(function() { fireRedLaser(); }, 6000);
+    function dblbeat_3() {
+      setTimeout(function() { fireRedLaser(); }, 00);
+      setTimeout(function() { fireGreenLaser(); }, 125);
+    }
 
-//     if (lastPhrase === 1) {
-//       setTimeout(function() { fireBlueLaser(); }, 6000);
-//       setTimeout(function() { fireGreenLaser(); }, 6250);
-//       setTimeout(function() { fireRedLaser(); }, 6500);
-//       setTimeout(function() { fireBlueLaser(); }, 6750);
-//       setTimeout(function() { fireRedLaser(); }, 7000);
-//     };
-//     lastPhrase++;
-//   }
-//   setTimeout(function() { outro(); }, 63000);
-//   setTimeout(function() { outro(); }, 71000);
+    // Reuse this code to keep it DRY
+    function fire_dblbeats(currDblBeat, num) {
+      setTimeout(function() { fireBlueLaser(); }, 250);
 
-//   // Fire function to add score to player
-//   setTimeout(function() { postScore(playerScore, currentHighStreak); }, 81000);
-//   // display leaderboard with ajax request
-//   setTimeout(function() { getPlayers(playerScore, currentHighStreak); }, 82005);
-// };
+      setTimeout(function() { currDblBeat(); }, 500);
+      if (num === 0) {
+        setTimeout(function() { currDblBeat(); }, 875);
+      };
+      setTimeout(function() { currDblBeat(); }, 1250);
+      setTimeout(function() { currDblBeat(); }, 1625);
+    }
+
+    setTimeout(function() { fire_dblbeats(dblbeat_1, 0); }, 0);
+    setTimeout(function() { fire_dblbeats(dblbeat_2, 0); }, 2000);
+    setTimeout(function() { fire_dblbeats(dblbeat_3, 1); }, 4000);
+
+    // Continue to the second phrase
+    setTimeout(function() { fireBlueLaser(); }, 5000);
+    setTimeout(function() { fireGreenLaser(); }, 5250);
+    setTimeout(function() { fireRedLaser(); }, 5500);
+    setTimeout(function() { fireBlueLaser(); }, 5750);
+    setTimeout(function() { fireRedLaser(); }, 6000);
+
+    if (lastPhrase === 1) {
+      setTimeout(function() { fireBlueLaser(); }, 6000);
+      setTimeout(function() { fireGreenLaser(); }, 6250);
+      setTimeout(function() { fireRedLaser(); }, 6500);
+      setTimeout(function() { fireBlueLaser(); }, 6750);
+      setTimeout(function() { fireRedLaser(); }, 7000);
+    };
+    lastPhrase++;
+  }
+  setTimeout(function() { outro(); }, 63000);
+  setTimeout(function() { outro(); }, 71000);
+
+  // Fire function to add score to player
+  setTimeout(function() { postScore(playerScore, currentHighStreak); }, 81000);
+  // display leaderboard with ajax request
+  setTimeout(function() { getPlayers(playerScore, currentHighStreak); }, 82005);
+};
 
 
 
