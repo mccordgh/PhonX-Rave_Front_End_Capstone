24c082ae4d8dd87876c7b8af56a27810df3c65f4
williamhickok11 <williamhickok11@gmail.com>
2016-10-07 13:58:07 -0500

    trying to figure out how to paws the game

--- app/js/Songs/fade.js
+++ app/js/Songs/fade.js
@@ -18,10 +18,45 @@ var fireFade = function(timing, note) {
     // postScore(playerScore, currentHighStreak, "pioneer66_")
 }
 
+// var x = 1;
+// var intervalID = setInterval(function(){
+//     if (x >= 5000) {
+//         clearInterval(intervalID);
+//     };
+//     console.log("x",x);
+//     x+= 1000;
+// }, (1000));
 
 
+function trial(note, timing){
+    console.log("note",note);
+    if (note === "J") {
+        setTimeout(function(){fire_J_Laser();}, (4.67*timing));
+    };
+    if (note === "K") {
+        setTimeout(function(){fire_K_Laser();}, (4.67*timing));
+    };
+    if (note === "L") {
+        setTimeout(function(){fire_L_Laser();}, (4.67*timing));
+    };
+}
 
 
+function interval(func, wait, times){
+    var interv = function(w, t){
+        return function(){
+            if(typeof t === "undefined" || t-- > 0){
+                setTimeout(interv, w);
+                func("J", 2000);
+            }
+        };
+    }(wait, times);
+
+    setTimeout(interv, wait);
+    wait += 1000;
+};
+
+interval(trial, 1000, 3);
 
 
 
